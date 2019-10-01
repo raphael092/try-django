e39e9be645fd3d2b4504993b90e333e54b0ef505
Raphael Rieble <raphael.rieble@gmail.com>
2019-10-01 13:56:31 +0200

    add blog-app and models

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -5,10 +5,16 @@ from django.template.loader import get_template
 
 def home_page(request):
     my_title = "Hello there..."
+    context = {"title": my_title}
+    if request.user.is_authenticated:
+        context = {"title": my_title, "my_list": [1, 2 ,3 ,4, 5]}
+    ##template_name = "title.txt"
+    ##template_obj = get_template(template_name)
+    ##rendered_string = template_obj.render(context)
     #doc = "<h1>{title}</h1>".format(title=title)
     #django_rendered_doc = "<h1>{{title}}</h1>".format(title=title)
     #return HttpResponse("<h1>#bethebestyou</h1>")
-    return render(request, "hello_world.html", {"title": my_title})
+    return render(request, "home.html", context)
 
 def about_page(request):
     return render(request, "about.html", {"title": "About us"})
@@ -17,9 +23,9 @@ def contact_page(request):
     return render(request, "about.html", {"title": "Contact us"})
      
 #def example_page(request):              #returns "hello_world.html"
-    context = {"title": "Example"}
-    something_here = "hello_world.html"
-    return HttpResponse(something_here)
+   # context = {"title": "Example"}
+   # something_here = "hello_world.html"
+   # return HttpResponse(something_here)
 
 def example_page(request):              #returns an item"
     context         = {"title": "Example"}
